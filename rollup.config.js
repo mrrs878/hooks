edd0c306fe630000af9d191629378af7f2a8406f
lihang.gw <lihang.gw@heyqu.net>
2020-12-11 11:09:41 +0800

    开发：完善rollup配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,41 @@
 /*
  * @Author: your name
  * @Date: 2020-12-10 19:11:10
- * @LastEditTime: 2020-12-10 23:44:18
+ * @LastEditTime: 2020-12-11 11:03:13
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \jsLibrary\rollup.config.js
  */
-import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import path from 'path';
-import ts from '@rollup/plugin-typescript';
-import { terser } from "rollup-plugin-terser";
+import ts from 'rollup-plugin-typescript2';
+import cleaner from 'rollup-plugin-cleaner';
 
 export default {
-  input: {
-    'native': './src/native/index.ts',
-    'react': './src/react/index.ts'
-  },
-  output:{
-    dir: 'dist',
-    format: 'esm',
-  },
+  input: './src/index.ts',
+  output: [
+    {
+      dir: 'dist/esm',
+      format: 'esm',
+    },
+    {
+      dir: 'dist/iife',
+      name: 'MJSLibrary',
+      format: 'iife',
+    },
+  ],
   plugins: [
+    cleaner({
+      targets: [
+        './dist/'
+      ]
+    }),
     resolve({ 
       extensions: ['.ts', '.tsx', '.js', '.jsx'],
     }),
     commonjs(),
     ts({ tsconfig: path.resolve(__dirname, 'tsconfig.json') }),
-    terser(),
   ],
   external: ['react']
 }
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: mrrs878
+ * @Date: 2020-12-11 10:27:21
+ * @LastEditTime: 2020-12-11 10:28:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \jsLibrary\src\index.ts
+ */
+import * as native from './native';
+import * as react from './react';
+
+export { native, react };
