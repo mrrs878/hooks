82cd47688f765801019a78b0d23314b9451a9809
lihang.gw <lihang.gw@heyqu.net>
2020-12-11 12:01:08 +0800

    开发：修复babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-10 19:11:10
- * @LastEditTime: 2020-12-11 11:03:13
+ * @LastEditTime: 2020-12-11 11:30:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \jsLibrary\rollup.config.js
@@ -12,30 +12,44 @@ import path from 'path';
 import ts from 'rollup-plugin-typescript2';
 import cleaner from 'rollup-plugin-cleaner';
 
-export default {
-  input: './src/index.ts',
-  output: [
-    {
+const extensions = ['.ts', '.tsx', '.js', '.jsx'];
+const tsconfig = path.resolve(__dirname, 'tsconfig.json');
+
+export default [
+  {
+    input: './src/index.ts',
+    output: {
       dir: 'dist/esm',
       format: 'esm',
     },
-    {
-      dir: 'dist/iife',
+    plugins: [
+      cleaner({ targets: ['./dist/'] }),
+      resolve({ extensions }),
+      commonjs(),
+      ts({ tsconfig }),
+    ],
+    external: ['react']
+  },
+  {
+    input: './src/index.ts',
+    output: {
+      dir: 'dist/umd',
+      format: 'umd',
       name: 'MJSLibrary',
-      format: 'iife',
+      globals: {
+        react: 'React'
+      }
     },
-  ],
-  plugins: [
-    cleaner({
-      targets: [
-        './dist/'
-      ]
-    }),
-    resolve({ 
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    }),
-    commonjs(),
-    ts({ tsconfig: path.resolve(__dirname, 'tsconfig.json') }),
-  ],
-  external: ['react']
-}
+    plugins: [
+      resolve({ 
+        extensions
+      }),
+      commonjs(),
+      ts({ 
+        tsconfigOverride: { compilerOptions: { declaration: false } }, 
+        tsconfig
+      }),
+    ],
+    external: ['react']
+  }
+]
