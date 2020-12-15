80c0c0a08ee9bea1ddfea593a7da112123fb90db
mrrs878 <mrrs878@foxmail.com>
2020-12-15 23:51:55 +0800

    开发：使用@testing-library/react测试hook

--- test/useImgLazyLoad.test.tsx
+++ test/useImgLazyLoad.test.tsx
@@ -1,31 +1,28 @@
 /*
  * @Author: your name
  * @Date: 2020-12-14 10:37:22
- * @LastEditTime: 2020-12-14 22:52:45
+ * @LastEditTime: 2020-12-15 23:49:24
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \jsLibrary\test\useImgLazyLoad.test.tsx
  */
 import React, { useEffect } from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 import useImgLazyLoad from '../src/react/useImgLazyLoad';
 
 test('test useImgLazyLoad', async () => {
-  const App = () => {
-    const [loading, imgUrl, errMsg] = useImgLazyLoad({ imgUrl: 'https://mrrsblog.oss-cn-shanghai.aliyuncs.com/avatar.jpg', backupImgUrl: "333" });
-    useEffect(() => {
-      console.log(loading);
-      
-    }, [loading])
-    return (
-      <div>
-        <img src={imgUrl} title="img" srcSet="" />
-        <span title="span">{errMsg}</span>
-      </div>
-    )
-  }
-  render(<App />);
-  // const img = await screen.findByTitle("img", {}, { timeout: 2000 });
-  // const span = await screen.findByTitle("span", {}, { timeout: 2000 });
+  const { result } = renderHook(
+    () => useImgLazyLoad({ imgUrl: 'https://mrrsblog.oss-cn-shanghai.aliyuncs.com/avatar.jpg', backupImgUrl: "333" }),
+  )
+  const [loading, imgUrl] = result.current;
+  await waitFor(
+    () => loading === false,
+    { timeout: 3000, onTimeout: (e) => {
+      console.log(e);
+      return e;
+    } }
+  );
+  // expect(imgUrl).toBe('https://mrrsblog.oss-cn-shanghai.aliyuncs.com/avatar.jpg');
   expect(1).toBe(1);
 })
