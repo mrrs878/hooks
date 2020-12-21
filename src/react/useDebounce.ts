a9c724f6f584780450226bdc469ad7635b920a71
lihang.gw <lihang.gw@heyqu.net>
2020-12-21 22:48:02 +0800

    开发:优化useDebounce

--- src/react/useDebounce.ts
+++ src/react/useDebounce.ts
@@ -1,12 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2020-12-21 18:01:32
- * @LastEditTime: 2020-12-21 19:06:39
+ * @LastEditTime: 2020-12-21 22:46:52
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \jsLibrary\src\react\useDebounce.ts
  */
 import _, { DebouncedFunc } from 'lodash';
+import { useMemo, useRef } from 'react';
 
 const { debounce } = _;
 
@@ -20,8 +21,8 @@ interface PropsI {
 }
 function useDebounce<T extends Fn>(fn: T, options?: PropsI): [T, () => void, () => void] {
   const wait = options?.wait || 1000;
-  const debounced = debounce<T>(fn, wait, options);
-  return [debounced as unknown as T, debounced.cancel, debounced.flush];
+  const debounced = debounce<T>(((...args: Array<[]>) => fn(...args)) as T, wait, options);
+  return [(debounced as unknown) as T, debounced.cancel, debounced.flush];
 }
 
 export default useDebounce;
--- test/useDebounce.test.tsx
+++ test/useDebounce.test.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-12-21 18:56:37
- * @LastEditTime: 2020-12-21 19:36:46
+ * @LastEditTime: 2020-12-21 22:40:33
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \jsLibrary\test\useDebounce.test.tsx
@@ -9,20 +9,20 @@
 import { renderHook,act } from '@testing-library/react-hooks';
 import { sleep } from '../src/tools';
 import useDebounce from '../src/react/useDebounce';
+import { debounce } from 'lodash';
 
 describe('test useDebounce', () => {
-  const fn = (param: number) => param += 1;
+  const fn = (param: number) => param + 1;
   test('run', async () => {
     let cnt = 0;
     const { result } = renderHook(() => useDebounce(fn));
     const [run] = result.current;
   
     await act(async () => {
-      run(cnt);
-      run(cnt);
-      run(cnt);
+      cnt = run(cnt) || 0;
       expect(cnt).toBe(0);
-      await sleep(1000);
+      await sleep(1100);
+      cnt = run(cnt);
       expect(cnt).toBe(1);
     })
   }),
@@ -31,13 +31,15 @@ describe('test useDebounce', () => {
     const { result } = renderHook(() => useDebounce(fn, { wait: 2000 }));
     const [run] = result.current;
 
-    run(cnt);
-    run(cnt);
-    run(cnt);
-    expect(cnt).toBe(0);
-    await sleep(1000);
-    expect(cnt).toBe(0);
-    await sleep(1100);
-    expect(cnt).toBe(1);
+    await act(async () => {
+      cnt = run(cnt) || 0;
+      expect(cnt).toBe(0);
+      await sleep(1000);
+      cnt = run(cnt) || 0;
+      expect(cnt).toBe(0);
+      await sleep(2100);
+      cnt = run(cnt) || 0;
+      expect(cnt).toBe(1);
+    })
   })
 })
