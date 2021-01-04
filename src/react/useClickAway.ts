d90756b57c393325a2511d863c71d58541e6df94
lihang.gw <lihang.gw@heyqu.net>
2021-01-04 19:24:15 +0800

    开发：添加useClickAway

---
+++ src/react/useClickAway.ts
@@ -0,0 +1,38 @@
+/*
+* @Author: your name
+* @Date: 2020-12-31 12:47:00
+ * @LastEditTime: 2020-12-31 12:58:25
+ * @LastEditors: Please set LastEditors
+* @Description: In User Settings Edit
+* @FilePath: /jsLibrary/src/react/useClickAway.ts
+*/
+import { useEffect, useRef } from 'react';
+import { BasicTarget, getTargetElement } from '../tools/dom';
+
+const defaultEvent = 'click';
+
+type EventType = MouseEvent | TouchEvent;
+
+function useClickAway(onClickAway: (event: EventType) => void, target: BasicTarget | Array<BasicTarget>, eventName: string = defaultEvent) {
+  const onClickAwayRef = useRef(onClickAway);
+  onClickAwayRef.current = onClickAway;
+
+  useEffect(() => {
+    const handler = (event: any) => {
+      const targets = Array.isArray(target) ? target : [target];
+      if (
+        targets.some((targetItem) => {
+          const targetElement = getTargetElement(targetItem) as HTMLElement;
+          return !targetElement || targetElement?.contains(event.target);
+        })
+      ) return;
+      onClickAwayRef.current(event);
+    };
+
+    document.addEventListener(eventName, handler);
+
+    return () => {
+      document.removeEventListener(eventName, handler);
+    };
+  }, [target, eventName]);
+}
---
+++ src/tools/dom.ts
@@ -0,0 +1,35 @@
+import { MutableRefObject } from 'react';
+
+/*
+ * @Author: your name
+ * @Date: 2020-12-31 12:48:41
+ * @LastEditTime: 2020-12-31 12:56:06
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /jsLibrary/src/tools/dom.ts
+ */
+export type BasicTarget<T = HTMLElement> =
+  | (() => T | null)
+  | T
+  | null
+  | MutableRefObject<T | null | undefined>;
+
+type TargetElement = HTMLElement | Element | Document | Window;
+
+export function getTargetElement(
+  target?: BasicTarget<TargetElement>,
+  defaultElement?: TargetElement,
+): TargetElement | undefined | null {
+  if (!target) return defaultElement;
+  let targetElement: TargetElement | undefined | null;
+
+  if (typeof target === 'function') {
+    targetElement = target();
+  } else if ('current' in target) {
+    targetElement = target.current;
+  } else {
+    targetElement = target;
+  }
+
+  return targetElement;
+}
