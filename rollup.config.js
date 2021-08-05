af0b6ac68325b1ce55f33be2aeb659820111f72a
echo_li1 <echo_li1@yunquna.com>
2021-08-05 10:08:53 +0800

    build: 发布新版本

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 /*
  * @Author: mrrs878@foxmail.com
  * @Date: 2020-12-10 19:11:10
- * @LastEditTime: 2020-12-11 11:30:24
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-08-05 09:58:33
+ * @LastEditors: mrrs878@foxmail.com
  * @Description: In User Settings Edit
- * @FilePath: \jsLibrary\rollup.config.js
+ * @FilePath: d:\Data\Personal\MyPro\js_library\rollup.config.js
  */
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
@@ -24,7 +24,7 @@ export default [
     },
     plugins: [
       cleaner({ targets: ['./dist/'] }),
-      resolve({ extensions }),
+      resolve({ extensions, rootDir: './src' }),
       commonjs(),
       ts({ tsconfig }),
     ],
